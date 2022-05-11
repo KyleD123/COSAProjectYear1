aed83beee57222c10e02d753a58452b0c2ac7e1c
Wade Lahoda <ins212@cst.siast.sk.ca>
2022-05-11 11:02:16 -0600

    Initial commit of project --Wade

---
+++ src/main/java/com/cosacpmg/App.java
@@ -0,0 +1,31 @@
+package com.cosacpmg;
+
+import javafx.application.Application;
+import javafx.fxml.*;
+import javafx.scene.*;
+import javafx.scene.control.*;
+import javafx.stage.*;
+import java.io.IOException;
+public class App extends Application
+{
+    @Override
+    public void start(Stage stage) throws IOException {
+        FXMLLoader fxmlLoader = new FXMLLoader(App.class.getResource("app-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setTitle("Hello!");
+        stage.setScene(scene);
+        stage.show();
+    }
+
+    public static void main(String[] args) {
+        launch();
+    }
+
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
