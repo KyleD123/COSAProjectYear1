5508a5437b00e94fd1c2abaa9629943de89d50dd
Taylor <stigen8209@saskpolytech.ca>
2022-05-16 10:49:57 -0600

    Taylor/Matt worked on filling out Player Object

--- src/main/java/models/Player.java
+++ src/main/java/models/Player.java
@@ -9,22 +9,45 @@ import java.io.Serializable;
 @DatabaseTable(tableName = "players")
 public class Player implements Serializable {
 
+    @DatabaseField(generatedId = true)
+    private long lPlayerID;
+
     @DatabaseField(canBeNull = false)
     @Size(max = 15, message="Player First Name Too Long")
     @NotEmpty(message ="Player First Name is Required")
     private String sFirstName;
-    
-            sLastName, sPosition, sParentInfo, sEmergencyContact;
+
+    @DatabaseField(canBeNull = false)
+    @Size(max = 15, message = "Player Last Name Too Long")
+    @NotEmpty(message = "Player Last Name is Required")
+    private String sLastName;
+
+    @DatabaseField(canBeNull = false)
+    @Size(min = 2, max = 2, message = "Player Number must be 2 digits")
+    @NotEmpty(message = "Player Number is Required")
     private int nPlayerNumber;
 
-    public Player(String sFirstName, String sLastName, String sPosition, String sParentInfo, String sEmergencyContact, int nPlayerNumber) {
-        this.sFirstName = sFirstName;
-        this.sLastName = sLastName;
-        this.sPosition = sPosition;
-        this.sParentInfo = sParentInfo;
-        this.sEmergencyContact = sEmergencyContact;
-        this.nPlayerNumber = nPlayerNumber;
+    @DatabaseField(canBeNull = false)
+    @Size(max = 20, message = "Player Position Too Long")
+    @NotEmpty(message = "Player Position is Required")
+    private String sPosition;
+
+    @DatabaseField(canBeNull = false)
+    @Size(max = 30, message = "Player Parent Info Too Long")
+    @NotEmpty(message = "Player Parent Info is Required")
+    private String sParentInfo;
+
+    @DatabaseField(canBeNull = false)
+    @Size(max = 14, min = 12, message = "Invalid Phone Number")
+    @Pattern(regexp = "[0-9]?[-]?[0-9]{3}[-][0-9]{3}[-][0-9]{4}", message = "Improper Phone Number Format")
+    private String sEmergencyContact;
+
+    public Player() {
     }
+
+    public long getlPlayerID() { return lPlayerID; }
+
+    public void setlPlayerID(long playerID) { this.lPlayerID = playerID; }
 
     public String getsFirstName() {
         return sFirstName;
@@ -72,5 +95,15 @@ public class Player implements Serializable {
 
     public void setnPlayerNumber(int nPlayerNumber) {
         this.nPlayerNumber = nPlayerNumber;
+    }
+
+    public void setAllValues(Player player) {
+        this.setlPlayerID(player.getlPlayerID());
+        this.setsFirstName(player.getsFirstName());
+        this.setsLastName(player.getsLastName());
+        this.setnPlayerNumber(player.getnPlayerNumber());
+        this.setsPosition(player.getsPosition());
+        this.setsParentInfo(player.getsParentInfo());
+        this.setsEmergencyContact(player.getsEmergencyContact());
     }
 }
