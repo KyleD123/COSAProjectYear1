3c51c2d925a513598ee623966c4b688feb13d525
unknown <cst109@CST.SIAST.SK.CA>
2022-05-16 08:56:07 -0600

    Today Gabe(co-pilot) and Kyle(pilot) started the Tournament object annotations as well as adding the dependencies into the pom.xml File

--- src/main/java/models/Tournament.java
+++ src/main/java/models/Tournament.java
@@ -1,4 +1,75 @@
 package models;
 
-public class Tournament {
+import java.io.Serializable;
+import java.util.Date;
+
+
+
+//@DatabaseTable(tableName = "tournaments")
+public class Tournament implements Serializable
+{
+    //ano's
+
+    private int tournamentId;
+
+    //annotations for tournament name
+
+    private String tournamentName;
+
+    //ano's for start date
+    private Date startDate;
+
+    //ano's for end data
+    private Date endDate;
+
+
+    public Tournament()
+    {
+
+    }
+
+    public int getTournamentId()
+    {
+
+        return this.tournamentId;
+    }
+
+    public void setTournamentId(int nId)
+    {
+        this.tournamentId = nId;
+    }
+
+    public String getTournamentName()
+    {
+        return tournamentName;
+    }
+
+    public void setTournamentName(String sName)
+    {
+        this.tournamentName = sName;
+    }
+
+    public Date getStartDate()
+    {
+        return this.startDate;
+    }
+
+    public void setStartDate(Date start)
+    {
+        this.startDate = start;
+    }
+
+    public Date getEndDate()
+    {
+        return this.endDate;
+    }
+
+    public void setEndDate(Date end)
+    {
+        this.endDate = end;
+    }
+
+
+
+
 }
