0e12d477f5146616f3cf00066a83a761a9d91432
Matt T <truong5615@saskpolytech.ca>
2022-05-17 14:54:39 -0600

    Taylor and I finish the GUI, now working on coding the controller

--- src/main/java/controllers/PlayerController.java
+++ src/main/java/controllers/PlayerController.java
@@ -1,27 +1,53 @@
 package controllers;
 
 import com.j256.ormlite.dao.Dao;
+import com.j256.ormlite.dao.DaoManager;
 import com.j256.ormlite.support.ConnectionSource;
+import com.j256.ormlite.table.TableUtils;
 import models.Player;
 
 import java.sql.Connection;
+import java.sql.SQLException;
 
 public class PlayerController {
     private Dao<Player, Long> repo;
 
     public PlayerController(ConnectionSource databaseConn) {
-
+        try {
+            this.repo = DaoManager.createDao(databaseConn, Player.class);
+            repo.setAutoCommit(databaseConn.getReadWriteConnection("players"), true);
+            TableUtils.createTableIfNotExists(databaseConn, Player.class);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
     }
 
     public boolean createPlayer(Player obPlayer) {
-        return true;
+        if (verifyUnique(obPlayer)) {
+            try {
+                int result = repo.create(obPlayer);
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 
     public boolean verifyUnique(Player obPlayer) {
+
         return true;
     }
 
     public boolean modifyPlayer(Player obPlayer) {
-        return true;
+        if (verifyUnique(obPlayer)) {
+            try {
+                int result = repo.update(obPlayer);
+                return true;
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return false;
     }
 }
