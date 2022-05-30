42648e0bbc56098e0f7ee7988a61e674f02918f6
Kyle Doerksen <doerksen8556@saskpolytech.ca>
2022-05-30 08:53:51 -0600

    Kyle Pilot, & Jared Co-Pilot, worked on setting up and annotating all the test cases, we then created the Roster and Roster controller class and got started on annotating. We also worked giving ids to the layout text fields that we are using in our story.

---
+++ src/main/java/models/Roster.java
@@ -0,0 +1,15 @@
+package models;
+
+import com.j256.ormlite.table.DatabaseTable;
+import javafx.scene.control.TextField;
+
+@DatabaseTable(tableName = "players")
+public class Roster
+{
+
+    private TextField txtGoalie, txtRightDefense, txtLeftDefense, txtRightWing, txtLeftWing, txtCenter;
+
+    
+
+
+}
