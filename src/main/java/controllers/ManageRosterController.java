4eb21423c5049bbd31ad10e345cffb40dac07775
Matt T <truong5615@saskpolytech.ca>
2022-06-01 12:58:57 -0600

    just working on cleaning up code and working to get the player removed from the team

--- src/main/java/controllers/ManageRosterController.java
+++ src/main/java/controllers/ManageRosterController.java
@@ -7,6 +7,7 @@ import javafx.fxml.FXML;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.scene.Scene;
+import javafx.scene.control.Alert;
 import javafx.scene.control.ListView;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.DragEvent;
@@ -37,17 +38,16 @@ public class ManageRosterController implements Initializable
     @FXML
     private FlowPane fpBench;
 
-    @FXML
-    private ImageView Home;
-
-    int nCounter =1;
-
     private PlayerController playerControl;
 
     public HashMap<Text, Player> playerMap = new HashMap<>();
 
+    //only need this method if we are repopulating the team list when re-selecting a team
     private List<Player> listOfPlayerOnTeam = new ArrayList<>();
-//    private Text Text txtPlayer = new Text();
+    private ArrayList<String> selectedPlayers = new ArrayList<>();
+
+    private Text txtPlayer;
+
 
 
     @Override
@@ -65,7 +65,8 @@ public class ManageRosterController implements Initializable
 
         playerControl = new PlayerController(databaseConn);
         populateListView();
-//        populateTeamList();
+        //only need this method if we are repopulating the team list when re-selecting a team
+      populateTeamList();
 
     }
 
@@ -96,39 +97,39 @@ public class ManageRosterController implements Initializable
     public void addPlayerToTeam() throws SQLException {
 
         Player obSelected = PlayerList.getSelectionModel().getSelectedItem();
+        obSelected.setsTeamName(TeamViewController.obCurrentTeam);
         if(playerControl.modifyPlayer(obSelected))
         {
-
-            Text txtPlayer = new Text();
-            txtPlayer.setText("              "+obSelected.toString()+"               ");
+            txtPlayer = new Text();
+            txtPlayer.setText(obSelected.toString());
             txtPlayer.setOnDragDetected(this::setOnDragDetected);
             txtPlayer.setOnDragOver(this::setOnDragOver);
             txtPlayer.setOnDragDropped(this::setOnDragDropped);
             txtPlayer.setOnDragDone(this::setOnDraggedDone);
-//         txtPlayer.setOnMouseClicked(this::setOnMouseClicked);
-            txtPlayer.setId(("text" + nCounter));
+            txtPlayer.setOnMouseClicked(e -> {setOnMouseClicked(e,txtPlayer);});
 
-
-            fpBench.getChildren().add( txtPlayer);
-            playerMap.put( txtPlayer,obSelected);
-
-            obSelected.setsTeamName(TeamViewController.obCurrentTeam);
+            fpBench.getChildren().add(txtPlayer);
+            fpBench.setHgap(25);
+            playerMap.put(txtPlayer,obSelected);
+            populateListView();
+            //only need this if we are repopulating the team list when re-selected a team
+            listOfPlayerOnTeam.add(obSelected);
         }
-
         else
         {
-
+            Alert alert = new Alert(Alert.AlertType.INFORMATION);
+            alert.setTitle("Error!");
+            alert.setHeaderText(null);
+            alert.setContentText("Player was not modified in the database! Please try again.");
+            alert.show();
         }
-
-        populateListView();
-
-        listOfPlayerOnTeam.add(obSelected);
-
-        nCounter++;
     }
 
-    private void setOnMouseClicked(MouseEvent mouseEvent)
+    private void setOnMouseClicked(MouseEvent mouseEvent, Text txtPlayer)
     {
+        System.out.println("Gabe is shit");
+        txtPlayer.setFill(Color.RED);
+        selectedPlayers.add(txtPlayer.getText());
 
 
     }
@@ -147,34 +148,36 @@ public class ManageRosterController implements Initializable
 
     /**
      * This method is used to repopulate a team once the program is closed or if someone goes back to the main
-     * page, it will show all the players that are on that selected team again once selected
+     * page, and the team is reselected to manage the roster
+     * it will show all the players that are on that selected team again
      *
      * @param
      * @return
      */
-//    public void populateTeamList()
-//    {
-//        listOfPlayerOnTeam = playerControl.getAllPlayers().stream().filter(x -> x.getsTeamName().equals(TeamViewController.obCurrentTeam.toString())).collect(Collectors.toList());
-//        for (int i = 0; i<listOfPlayerOnTeam.size(); i++)
-//        {
-//            if (listOfPlayerOnTeam.get(i).getsTeamName().equals(TeamViewController.obCurrentTeam))
-//            {
-//                Player obSelected = listOfPlayerOnTeam.get(i);
-//                Text txtPlayer = new Text();
-//                 txtPlayer.setText(obSelected.toString());
-//
-//                txtPlayer.setOnDragDetected(this::setOnDragDetected);
-//                txtPlayer.setOnDragOver(this::setOnDragOver);
-//                txtPlayer.setOnDragDropped(this::setOnDragDropped);
-//                txtPlayer.setOnDragDone(this::setOnDraggedDone);
-//                 txtPlayer.setOnMouseClicked(this::setOnMouseClicked);
-////                 txtPlayer.setId(("text"+nCounter));
-//
-//                fpBench.getChildren().add( txtPlayer);
-//                playerMap.put(txtPlayer,obSelected);
-//            }
-//        }
-//    }
+    //only need this method if we are repopulating the team list when re-selecting a team
+
+    public void populateTeamList()
+    {
+        listOfPlayerOnTeam = playerControl.getAllPlayers().stream().filter(x -> x.getsTeamName().equals(TeamViewController.obCurrentTeam.toString())).collect(Collectors.toList());
+        for (int i = 0; i<listOfPlayerOnTeam.size(); i++)
+        {
+            if (listOfPlayerOnTeam.get(i).getsTeamName().equals(TeamViewController.obCurrentTeam))
+            {
+                Player obSelected = listOfPlayerOnTeam.get(i);
+                Text txtPlayer = new Text();
+                txtPlayer.setText(obSelected.toString());
+
+                txtPlayer.setOnDragDetected(this::setOnDragDetected);
+                txtPlayer.setOnDragOver(this::setOnDragOver);
+                txtPlayer.setOnDragDropped(this::setOnDragDropped);
+                txtPlayer.setOnDragDone(this::setOnDraggedDone);
+                txtPlayer.setOnMouseClicked(e -> {setOnMouseClicked(e,txtPlayer);});
+
+                fpBench.getChildren().add( txtPlayer);
+                playerMap.put(txtPlayer,obSelected);
+            }
+        }
+    }
 
     /**
      * This method is to go back to the main team window
